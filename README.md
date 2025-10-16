b1bde911d193ddac1705c8d0b35374cc5d52dd99
Vitor Dalmagro <vitordalmagro77@gmail.com>
2025-10-16 20:06:47 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Aprendendo a fazer um projeto robusto do zero
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
-function Home(){
-    return <h1>Família, obrigado por me apoiar todos os dias. Estou com saudades e amo vocês! Se vocês também me amam, deem uma risadinha aí kkk.</h1>
+function Home() {
+  return (
+    <h1>
+      Família, obrigado por me apoiar todos os dias. Estou com saudades e amo
+      vocês! Se vocês também me amam, deem uma risadinha aí kkk.
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("Teste");
+}
+
+function teste2() {
+  console.log("Identação errada");
 }
 
 export default Home;
