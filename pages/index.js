f4fcd2ac2a42692890b693ba1355a66241d15c80
Vitor Dalmagro <vitordalmagro77@gmail.com>
2025-10-17 23:09:22 +0000

    alterar a frase para teste

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ function Home() {
     <h1>
       Família, obrigado por me apoiar todos os dias. Estou com saudades e amo
       vocês! Se vocês também me amam, deem uma risadinha aí kkk.
+      testandoooooooooooooooooooo
     </h1>
   );
 }
