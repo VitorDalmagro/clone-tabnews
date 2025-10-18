32b6df30c6539b088c4e2d05134500ffa9b0ddac
Vitor Dalmagro <vitordalmagro77@gmail.com>
2025-10-18 17:10:06 +0000

    msg pro luizas

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,5 @@
 function Home() {
-  return (
-    <h1>
-      Família, obrigado por me apoiar todos os dias. Estou com saudades e amo
-      vocês! Se vocês também me amam, deem uma risadinha aí kkk.
-      testandoooooooooooooooooooo
-    </h1>
-  );
+  return <h1>Tu é man?</h1>;
 }
 
 export default Home;
